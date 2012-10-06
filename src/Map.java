618247f4e54bc19fdb48a9cc1e4cebce7a9a564b
henrik-s <henrik.sohlberg@gmail.com>
2012-10-06 14:49:23 +0200

    map changes

--- src/Map.java
+++ src/Map.java
@@ -1,7 +1,7 @@
 
 /**
  * Map contains:
- * 		A char matrix of the board, where each position is
+ * 		- A char matrix of the map, where each position is
  * 			# = Wall
  * 			@ = Player
  * 			+ = Player on goal square
@@ -9,6 +9,10 @@
  * 			* = Box on goal
  * 			. = Goal on square
  * 			(space) = empty square
+ * 		
+ * 		- A list of all the boxes
+ * 		- The position 		. 
+ * 	
  *  
  * @author MacHenriks
  *
@@ -16,42 +20,61 @@
 
 public class Map {
 	
-	Map prev_map;
-	private char[][] board;
+	final Map prevMap;
+	private char[][] map;
 	private int rows, cols;
 	
-	public Map(char[][] newMap) {
-		board = newMap;
-	}
+
 	
+	/**
+	 * Constructor for creating the map for the first time.
+	 * The pointer prev_map will we null
+	 *  
+	 * @param rows = number of rows of the map
+	 * @param cols = number of columns of the map
+	 */
 	public Map(int rows, int cols) {
-		board = new char[rows][cols];
+		prevMap = null;
+		map = new char[rows][cols];
 		this.rows = rows;
 		this.cols = cols;
 	}
 	
 	/**
-	 * Insert a row of the board into board matrix
-	 * @param s = row string of the board
+	 * Constructor for creating a map from a corresponding map
+	 * with a new move.
+	 * 
+	 * @param fromMap = parent Map
+	 * @param withMove = new Move
+	 */
+	public Map(Map fromMap, Move withMove) {
+		prevMap = fromMap;
+		this.map = fromMap.getMap();
+	}
+	
+	
+	/**
+	 * Insert a row of the map into map matrix
+	 * @param s = row string of the map
 	 * @param cRow = current row
 	 */
 	public void insertRow(String s, int cRow) {
 		char current;
 		for(int i = 0; i < cols; i++) {
 			current = s.charAt(i);
-			board[cRow][i] = current;
+			map[cRow][i] = current;
 		}
 	}
 	
 	/**
-	 * Return a string of the board
+	 * Return a string of the map
 	 * @return
 	 */
 	public String print() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < rows; i++) {
 			for(int j = 0; j < cols; j++) {
-				sb.append(board[i][j]);
+				sb.append(map[i][j]);
 			}
 			sb.append('\n');
 		}
@@ -59,9 +82,7 @@ public class Map {
 		return sb.toString();
 	}
 	
-	public char[][] getBoard() {
-		return this.board;
+	public char[][] getMap() {
+		return this.map;
 	}
-	
-	//public void update
 }
