bc3076a765a11cf6e0fdc6da9f42b2ba9aa53441
henrik-s <henrik.sohlberg@gmail.com>
2012-10-06 15:00:01 +0200

    hej simost billinge

---
+++ src/Box.java
@@ -0,0 +1,29 @@
+/**
+ * A box in the game, holding information of 
+ * 		where the player is standing
+ * 		what box is supposed to be moved
+ * 		to what position the box will be pushed
+ * @author MacHenrik
+ *
+ */
+
+public class Box {
+	private final int id;
+	private int currentPosition;
+	
+	// Constructor
+	public Box(int id, int currentPosition) {
+		this.id = id; 
+		this.currentPosition = currentPosition;
+	}
+	
+	// Return the box's id
+	public int getID() {
+		return id;
+	}
+	
+	//Return the box's currentPosition
+	public int getPosition() {
+		return currentPosition;
+	}
+}
