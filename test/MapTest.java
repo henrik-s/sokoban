0e07a7e803687a089a7c38c6527f001a02103f5c
Simon Österman <simon.oesterman@gmail.com>
2012-10-06 19:41:54 +0200

    testmekk

--- test/MapTest.java
+++ test/MapTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class MapTest {
 	
-	static final int LEVELS = 1
+	static final int LEVELS = 5
 			;
 	
 	@Test
 	public void test() {
 		Reader reader = new Reader("test/all", LEVELS);
-		Map map = reader.getLevel(1);
+		Map map = reader.getLevel(5);
 		System.out.println(map.print());
 	}
 
