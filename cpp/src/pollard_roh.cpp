d76cdddb07a5cb61fd4a2be5019610acd746266f
martenpalsson <marten.palsson@gmail.com>
2012-10-19 17:29:40 +0200

    saving before merge

--- cpp/src/pollard_roh.cpp
+++ cpp/src/pollard_roh.cpp
@@ -1,15 +1,31 @@
 #include <iostream>
 #include "gcd.h"
 #include <math.h>
+#include <stdlib.h>
 using namespace std;
+
+
+
+
 int parseArgv(int argc, char ** argv){
-	if(argc != 2){
+	int a = 0;
+	
+	if(argc == 1){
+		cout << "Enter number to be factorized: ";
+		cin >> a;
+		cout << endl;
+	}else if(argc > 2){
 		cout << "Input must be exactly one integer" << endl;
 		return -1;
 	}else{
+		int a = atoi(argv[1]);
+	}
 
+	return a;
+}
 
 int main(int argc, char **argv){
-	int parseArcv(argc, argv);
-	
+	int arg = parseArgv(argc, argv);
+	cout << "Här kommer a: " << arg << endl;
+	return 0;
 }
