88301b863bdfbbb312b406ae8a4a9cf7ecbbe18b
Mårten Pålsson <marten.palsson@gmail.com>
2012-11-27 17:10:15 +0100

    hejsan hoppas

--- projekt2/src/TSP.java
+++ projekt2/src/TSP.java
@@ -3,7 +3,11 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import org.jfree.data.xy.XYSeries;
+
 public class TSP {
+	
+	XYSeries xy = new XYSeries("Average size");
 
 	TSP() {
 		Map map;
@@ -25,6 +29,7 @@ public class TSP {
 	public void solve(Map map) {
 		int[] tour = greedyTour(map);
 		print_tour(tour,map);
+		tour = twoOpt(tour, map);
 	}
 
 	public int[] greedyTour(Map map) {
@@ -55,22 +60,57 @@ public class TSP {
 		}
 		return tour;
 	}
+	
+	public int[] twoOpt(int[] tour, Map map){
+		int[] T = tour.clone();
+		int x1,x2,y1,y2;
+		for(x1 = 0;x1 < tour.length - 1; x1 ++){
+			x2 = x1 + 1;
+			for(y1 = x2; y1 < tour.length; y1++){
+				if(y1 == tour.length - 1)
+					y2 = 0;
+				else 
+					y2 = y1 + 1;
+				
+				if(Util.dist(x1, x2, map) + Util.dist(y1, y2, map) > 
+					Util.dist(x1, y1, map) + Util.dist(x2, y2, map)){
+					swap(x2,y2,T);
+					print_tour(T,map);
+				}
+			}
+		}
+		return T;
+	}
+
+	private void swap(int x2, int y2, int[] T) {
+		int tmp = 0;
+		while(x2 < y2){
+			tmp = T[y2];
+			T[y2] = T[x2];
+			T[x2] = tmp;
+			x2++;
+			y2--;
+		}
+		
+	}
 
 	public void print_tour(int[] tour, Map map) {
 //		int distance = 0;
 		for (int i = 0; i < tour.length; ++i) {
-			System.out.println(tour[i]);
+			System.out.print(tour[i] + " ");
 //			if(i <= tour.length - 2){
 //				System.out.println(Util.dist(map.nodes[tour[i]],map.nodes[tour[i+1]]));
 				//distance += Util.dist(map.nodes[tour[i]],map.nodes[tour[i+1]]);
 //			}
 		}
+		System.out.println();
+		System.out.println("Distance: " + Util.dist(tour, map));
 //		distance += Util.dist(map.nodes[tour[0]],map.nodes[tour[tour.length-1]]);
 //		System.out.println("Distance: " + distance);
 	}
 
 	public static void main(String argv[]) {
-		new TSP();
+		new TSP("test");
 	}
 
 }
--- projekt2/src/Util.java
+++ projekt2/src/Util.java
@@ -1,10 +1,43 @@
+import java.util.List;
+
+import javax.security.auth.login.CredentialException;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.general.DatasetChangeListener;
+import org.jfree.data.general.DatasetGroup;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 
 
 public class Util {
+	
+	
+	public static double dist(int[] tour, Map map){
+		double dist = 0;
+		int x1,x2;
+		for(x1 = 0; x1 < tour.length; x1++){
+			if(x1 == tour.length - 1)
+				x2 = 0;
+			else
+				x2 = x1 + 1;
+			
+			dist += dist(tour[x1],tour[x2],map);
+		}
+		return dist;
+	}
+	
+	public static double dist(int x, int y, Map map){
+		return dist(map.nodes[x],map.nodes[y]);
+	}
 	
 	public static double dist(Node a, Node b){
 		double distance = Math.sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
 		return distance;
 	}
-
 }
